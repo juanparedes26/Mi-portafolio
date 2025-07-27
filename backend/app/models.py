97c271a2509938b41c0c90dcda02998700601b7d
federico-serron <f.serron.umpierrez@gmail.com>
2025-07-27 20:45:01 +0930

    upgraded models.py to newest SQLalchemy version

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,14 +1,24 @@
 from app import db
+from sqlalchemy import String, Boolean, ForeignKey, DateTime, Integer, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, timezone
+from typing import Optional
+
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(255))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(60), nullable=False)
+    name: Mapped[str] = mapped_column(String(60), nullable=False)
+    last_login: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now(timezone.utc))
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
     
     def serialize(self):
         return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email
+            'id': self.id,
+            'email': self.email,
+            'name': self.name,
+            'last_login': self.last_login,
+            'is_active': self.is_active,
         }
+    
