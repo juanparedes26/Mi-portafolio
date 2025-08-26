143e35389af61c07896ece2f11c8fd8b06f69927
juanparedes26 <139470808+juanparedes26@users.noreply.github.com>
2025-08-26 09:38:32 -0500

    Primer models listo

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,18 +7,35 @@ from typing import Optional
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(60), nullable=False)
-    name: Mapped[str] = mapped_column(String(60), nullable=False)
-    last_login: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now(timezone.utc))
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
-    
+ 
     def serialize(self):
         return {
             'id': self.id,
-            'email': self.email,
-            'name': self.name,
-            'last_login': self.last_login,
-            'is_active': self.is_active,
+            'username': self.username
         }
     
+class Project(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(1000), nullable=False)
+    techs: Mapped[list[str]] = mapped_column(String(300), nullable=False) 
+    repo_url: Mapped[str] = mapped_column(String(300), nullable=False)
+    live_url: Mapped[Optional[str]] = mapped_column(String(300), nullable=True)
+    image_url: Mapped[Optional[str]] = mapped_column(String(300), nullable=True)
+    images: Mapped[list[str]] = mapped_column(String(1000), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'techs': self.techs.split(",") if self.techs else [],
+            'repo_url': self.repo_url,
+            'live_url': self.live_url,
+            'image_url': self.image_url,
+            'images': self.images.split(",") if self.images else [],
+            'created_at': self.created_at
+        }
